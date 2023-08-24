16cef3221b30e5795e84de2140decaee841adf8b
alexandre.f.ext@e-ai.com.br <alexandre.f.ext@e-ai.com.br>
2023-08-24 11:01:49 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alexandre Papanis on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
